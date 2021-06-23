e1b16c8e3724cefef78f3b6a326858dd0c48b7a2
ritik sahu <50456414+ritikkumarsahu@users.noreply.github.com>
2021-06-23 12:26:04 +0530

    added test file

---
+++ test.py
@@ -0,0 +1,18 @@
+from facematch.face import match
+
+f = open(Path to your first image, 'rb')
+data1 = f.read()
+f.close()
+
+f = open('path to your id image', 'rb')
+data2 = f.read()
+f.close()
+
+result, distance, data = match(data1, data2)
+
+f = open('out.png', 'wb')
+f.write(data)
+f.close()
+
+print(distance)
+print(result)
